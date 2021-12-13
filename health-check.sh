93d292b46ba457b203b50c52feecbfbe02ca1343
André Bauer <andre.bauer@staffbase.com>
2021-12-13 09:50:35 +0100

    switch to pod delete
    Signed-off-by: André Bauer <andre.bauer@staffbase.com>

--- health-check.sh
+++ health-check.sh
@@ -9,9 +9,7 @@ while true; do
     echo "Checking Patroni health..."
 
     if curl -s localhost:8008 | jq '.state' | grep -q "start failed"; then
-        echo "Patroni start failed :("
-        echo "Restarting container..."
-        exit 1
+        curl -X DELETE "https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_PORT_443_TCP_PORT}/api/v1/namespaces/${POD_NAMESPACE}/pods/$(hostname)" -k -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
     fi
 
     echo "Patroni health OK :)"
