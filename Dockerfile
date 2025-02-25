cec33d5d0fc557ff592fa4ff2de804439e18bee1
André Bauer <monotek@users.noreply.github.com>
2025-02-25 18:06:17 +0100

    Update self-heal.sh (#45)
    * Update self-heal.sh
    
    * add jq

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM curlimages/curl:8.12.1
 
+USER root
+# hadolint ignore=DL3018
+RUN  apk add --no-cache jq
+USER curl_user
+
 COPY self-heal.sh /self-heal.sh
 
 CMD ["/bin/sh"]
 
 #checkov:skip=CKV_DOCKER_2:We don't need Docker HEALTHCHECK in Kubernetes
-#checkov:skip=CKV_DOCKER_3:Image inherits curl_user form base image
 
 ENTRYPOINT ["/self-heal.sh"]
--- self-heal.sh
+++ self-heal.sh
@@ -3,19 +3,40 @@
 # patroni self heal
 #
 
+set -e
+
+SLEEP_TIME="60"
+
 trap exit TERM INT
 
+log() {
+	echo "$(date +'%d.%m.%Y %H:%M:%S') - ${1}"
+}
+
+deletepod() {
+	log "Deleting pod ${POD_NAME} in namespace ${POD_NAMESPACE}!"
+	echo "${PATRONI_STATE}" | jq
+	curl -k -X DELETE "https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_PORT_443_TCP_PORT}/api/v1/namespaces/${POD_NAMESPACE}/pods/${POD_NAME}" -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -H "Accept: application/json" -H "Content-Type: application/json"
+	exit 1
+}
+
+log "Checking Patroni health every ${SLEEP_TIME} seconds..."
+
 while true; do
-    echo "$(date +'%d.%m.%Y %H:%M:%S') - Checking Patroni health..."
+	sleep "${SLEEP_TIME}"
 
-    if curl -s localhost:8008 | grep -q '"state": "start failed"'; then
-        echo "$(date +'%d.%m.%Y %H:%M:%S') - Patroni start failed :("
-        echo "$(date +'%d.%m.%Y %H:%M:%S') - Deleting pod..."
-        curl -k -X DELETE "https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_PORT_443_TCP_PORT}/api/v1/namespaces/${POD_NAMESPACE}/pods/${POD_NAME}" --data '{"gracePeriodSeconds":0,"propagationPolicy":"Background"}' -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -H "Accept: application/json" -H "Content-Type: application/json"
-        exit 1
-    fi
+	PATRONI_STATE="$(curl -s localhost:8008)"
 
-    echo "$(date +'%d.%m.%Y %H:%M:%S') - Patroni health OK :)"
-    sleep 60
+	if [ -z "${PATRONI_STATE}" ]; then
+		log "Patroni state empty :("
+		exit 1
+	elif [ -n "$(echo "${PATRONI_STATE}" | jq 'select(.state == "start failed")')" ]; then
+		log "Patroni start failed :("
+		deletepod
+	elif [ -n "$(echo "${PATRONI_STATE}" | jq 'select(.role == "replica") | select(.replication_state == "running")')" ]; then
+		log "Patroni replica not streaming :("
+		deletepod
+	fi
 
+	log "Patroni health OK :)"
 done
