8f6a5b6b496f197226f324ad79649036bed59488
theBNT <68970778+theBNT@users.noreply.github.com>
2025-07-18 22:54:49 +0200

    append new args at the end only

--- self-heal.sh
+++ self-heal.sh
@@ -16,7 +16,7 @@ log() {
 deletepod() {
 	log "Deleting pod ${POD_NAME} in namespace ${POD_NAMESPACE}!"
 	echo "${PATRONI_STATE}" | jq
-	curl -k -X DELETE -s "https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_PORT_443_TCP_PORT}/api/v1/namespaces/${POD_NAMESPACE}/pods/${POD_NAME}" -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -H "Accept: application/json" -H "Content-Type: application/json" -w '%{http_code}\n' -o /dev/null
+	curl -k -X DELETE "https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_PORT_443_TCP_PORT}/api/v1/namespaces/${POD_NAMESPACE}/pods/${POD_NAME}" -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -H "Accept: application/json" -H "Content-Type: application/json" -s -o /dev/null -w '%{http_code}\n'
 	exit 1
 }
 
